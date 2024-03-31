6ee2d4886615a8aef018a9404a8f305c21ce6c5b
Lucas Borges <lucasvnborges@gmail.com>
2024-03-31 04:36:06 -0300

    division of all numbers in a vector

---
+++ Division.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<double> numbers = {10.1, 20.2, 30.3, 40.4};
+    double divisor = 2.3;
+
+    for (auto &num : numbers)
+    {
+        num /= divisor;
+    }
+
+    std::cout << "Numbers in vector after division: ";
+
+    for (const auto &num : numbers)
+    {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
