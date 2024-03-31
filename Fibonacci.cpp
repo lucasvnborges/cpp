acd669a661ee93f2aa4dd5b96f70442321a844fe
Lucas Borges <lucasvnborges@gmail.com>
2024-03-31 04:36:29 -0300

    Fibonacci sequence

---
+++ Fibonacci.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+void fibonacci(int n)
+{
+    int t1 = 0, t2 = 1, nextTerm = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (i == 1)
+        {
+            std::cout << t1 << ", ";
+            continue;
+        }
+
+        if (i == 2)
+        {
+            std::cout << t2 << ", ";
+        }
+
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+
+        if (i < n - 1)
+        {
+            std::cout << nextTerm << ", ";
+        }
+        else
+        {
+            std::cout << nextTerm;
+        }
+    }
+}
+
+int main()
+{
+    int n = 11;
+    std::cout << "Fibonacci sequence up to " << n << " terms: ";
+    fibonacci(n);
+    return 0;
+}
