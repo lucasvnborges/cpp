a87512d4a64cab43472b9a8952126704d409da2f
Lucas Borges <lucasvnborges@gmail.com>
2024-04-01 14:19:56 -0300

    calculate factorial of a number

---
+++ Factorial.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+long long factorial(int n)
+{
+    long long value = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        value *= i;
+    }
+
+    return value;
+}
+
+int main()
+{
+    int n;
+
+    std::cout << "Enter a positive integer: ";
+    std::cin >> n;
+
+    long long result = factorial(n);
+
+    std::cout << "The factorial of " << n << " is: " << result << std::endl;
+
+    return 0;
+}
