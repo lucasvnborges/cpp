f4a8e14c46ef17f691e7ba2e259ca1a869e9b9a9
Lucas Borges <lucasvnborges@gmail.com>
2024-04-11 09:42:15 -0300

    adjusts code formatting with clang-format

--- SimpleInterest.cpp
+++ SimpleInterest.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
 
-double simpleInterest(double P, double R, double T)
-{
-    return (P * R * T) / 100;
+double simpleInterest(double P, double R, double T) {
+  return (P * R * T) / 100;
 }
 
-int main()
-{
-    double P, R, T;
+int main() {
+  double P, R, T;
 
-    std::cout << "Enter the principal amount (P): ";
-    std::cin >> P;
+  std::cout << "Enter the principal amount (P): ";
+  std::cin >> P;
 
-    std::cout << "Enter the rate of interest (R) in %: ";
-    std::cin >> R;
+  std::cout << "Enter the rate of interest (R) in %: ";
+  std::cin >> R;
 
-    std::cout << "Enter the time period (T) in years: ";
-    std::cin >> T;
+  std::cout << "Enter the time period (T) in years: ";
+  std::cin >> T;
 
-    double interest = simpleInterest(P, R, T);
+  double interest = simpleInterest(P, R, T);
 
-    std::cout << "The simple interest is: " << interest << std::endl;
+  std::cout << "The simple interest is: " << interest << std::endl;
 
-    return 0;
+  return 0;
 }
