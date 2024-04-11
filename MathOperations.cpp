b9ee662bb539039d812702ca1cc01a32eb8ad52b
Lucas Borges <lucasvnborges@gmail.com>
2024-04-11 09:41:52 -0300

    adjusts code formatting with clang-format

--- MathOperations.cpp
+++ MathOperations.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 
-int main()
-{
-    double num1 = 45.6;
-    double num2 = 3.5;
+int main() {
+  double num1 = 45.6;
+  double num2 = 3.5;
 
-    double sum = num1 + num2;
-    double difference = num1 - num2;
-    double product = num1 * num2;
-    double quotient = num1 / num2;
+  double sum = num1 + num2;
+  double difference = num1 - num2;
+  double product = num1 * num2;
+  double quotient = num1 / num2;
 
-    std::cout << "Sum: " << sum << std::endl;
-    std::cout << "Difference: " << difference << std::endl;
-    std::cout << "Product: " << product << std::endl;
-    std::cout << "Quotient: " << quotient << std::endl;
+  std::cout << "Sum: " << sum << std::endl;
+  std::cout << "Difference: " << difference << std::endl;
+  std::cout << "Product: " << product << std::endl;
+  std::cout << "Quotient: " << quotient << std::endl;
 
-    return 0;
+  return 0;
 }
