360bba59789f84e27aa31359398aeb3ffcb54138
Lucas Borges <lucasvnborges@gmail.com>
2024-04-11 09:25:47 -0300

    simulates an iterator function

---
+++ IteratorFunction.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Iterator
+{
+    int state;
+
+public:
+    Iterator() : state(0) {}
+    int next()
+    {
+        return state++;
+    }
+};
+
+int main()
+{
+    Iterator gen;
+
+    std::cout << gen.next() << std::endl;
+    std::cout << gen.next() << std::endl;
+
+    return 0;
+}
