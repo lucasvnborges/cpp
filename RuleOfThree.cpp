ec1985dafe3f7e0451583109045fe529709d0bbd
Lucas Borges <lucasvnborges@gmail.com>
2024-04-11 09:42:12 -0300

    adjusts code formatting with clang-format

--- RuleOfThree.cpp
+++ RuleOfThree.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 
-double ruleOfThree(double a, double b, double c)
-{
-    return (b * c) / a;
+double ruleOfThree(double a, double b, double c) {
+  return (b * c) / a;
 }
 
-int main()
-{
-    double a = 2.0;
-    double b = 3.0;
-    double c = 4.0;
+int main() {
+  double a = 2.0;
+  double b = 3.0;
+  double c = 4.0;
 
-    std::cout << "The result of the rule of three is: " << ruleOfThree(a, b, c) << std::endl;
+  std::cout << "The result of the rule of three is: " << ruleOfThree(a, b, c) << std::endl;
 
-    return 0;
+  return 0;
 }
