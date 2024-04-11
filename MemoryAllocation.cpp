e2d9c3c77f3e4052dc305cdeef44f18871b3c3af
Lucas Borges <lucasvnborges@gmail.com>
2024-04-11 09:41:54 -0300

    adjusts code formatting with clang-format

--- MemoryAllocation.cpp
+++ MemoryAllocation.cpp
@@ -1,27 +1,24 @@
 #include <iostream>
 
-int main()
-{
-    int n = 5; // Número de elementos no array
+int main() {
+  int n = 5; // Número de elementos no array
 
-    // Aloca memória para o array
-    int *array = new int[n];
+  // Aloca memória para o array
+  int *array = new int[n];
 
-    // Preenche o array
-    for (int i = 0; i < n; i++)
-    {
-        array[i] = i;
-    }
+  // Preenche o array
+  for (int i = 0; i < n; i++) {
+    array[i] = i;
+  }
 
-    // Imprime o array
-    for (int i = 0; i < n; i++)
-    {
-        std::cout << array[i] << " ";
-    }
-    std::cout << std::endl;
+  // Imprime o array
+  for (int i = 0; i < n; i++) {
+    std::cout << array[i] << " ";
+  }
+  std::cout << std::endl;
 
-    // Libera a memória alocada para o array
-    delete[] array;
+  // Libera a memória alocada para o array
+  delete[] array;
 
-    return 0;
+  return 0;
 }
