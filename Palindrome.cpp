25731133e42875223aa52d1b786ce545cedb279a
Lucas Borges <lucasvnborges@gmail.com>
2024-04-11 09:41:57 -0300

    adjusts code formatting with clang-format

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,24 +1,19 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <string>
 
-bool isPalindrome(const std::string &str)
-{
-    std::string reversedStr = str;
-    std::reverse(reversedStr.begin(), reversedStr.end());
-    return str == reversedStr;
+bool isPalindrome(const std::string &str) {
+  std::string reversedStr = str;
+  std::reverse(reversedStr.begin(), reversedStr.end());
+  return str == reversedStr;
 }
 
-int main()
-{
-    std::string str = "anabana";
-    if (isPalindrome(str))
-    {
-        std::cout << str << " is a palindrome." << std::endl;
-    }
-    else
-    {
-        std::cout << str << " is not a palindrome." << std::endl;
-    }
-    return 0;
+int main() {
+  std::string str = "anabana";
+  if (isPalindrome(str)) {
+    std::cout << str << " is a palindrome." << std::endl;
+  } else {
+    std::cout << str << " is not a palindrome." << std::endl;
+  }
+  return 0;
 }
