9ca52cbcdbae9838b323655f6a03abb31d7b83c1
Lucas Borges <lucasvnborges@gmail.com>
2024-04-11 09:42:00 -0300

    adjusts code formatting with clang-format

--- PrimitiveTypes.cpp
+++ PrimitiveTypes.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 
-int main()
-{
-    int Int = 10;
-    short Short = 5;
-    long Long = 1000000;
-    long long LongLong = 1000000000000;
-    unsigned int UnsignedInt = 20;
-    float Float = 3.14;
-    double Double = 3.14159;
-    char Char = 'A';
-    bool Bool = true;
+int main() {
+  int Int = 10;
+  short Short = 5;
+  long Long = 1000000;
+  long long LongLong = 1000000000000;
+  unsigned int UnsignedInt = 20;
+  float Float = 3.14;
+  double Double = 3.14159;
+  char Char = 'A';
+  bool Bool = true;
 
-    std::cout << "int: " << Int << std::endl;
-    std::cout << "short: " << Short << std::endl;
-    std::cout << "long: " << Long << std::endl;
-    std::cout << "long long: " << LongLong << std::endl;
-    std::cout << "unsigned int: " << UnsignedInt << std::endl;
-    std::cout << "float: " << Float << std::endl;
-    std::cout << "double: " << Double << std::endl;
-    std::cout << "char: " << Char << std::endl;
-    std::cout << "bool: " << std::boolalpha << Bool << std::endl;
+  std::cout << "int: " << Int << std::endl;
+  std::cout << "short: " << Short << std::endl;
+  std::cout << "long: " << Long << std::endl;
+  std::cout << "long long: " << LongLong << std::endl;
+  std::cout << "unsigned int: " << UnsignedInt << std::endl;
+  std::cout << "float: " << Float << std::endl;
+  std::cout << "double: " << Double << std::endl;
+  std::cout << "char: " << Char << std::endl;
+  std::cout << "bool: " << std::boolalpha << Bool << std::endl;
 
-    return 0;
+  return 0;
 }
