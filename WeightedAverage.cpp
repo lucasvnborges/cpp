931fa5c52c67c525c0bbebb2a2cf853590d61765
Lucas Borges <lucasvnborges@gmail.com>
2024-04-11 09:42:17 -0300

    adjusts code formatting with clang-format

--- WeightedAverage.cpp
+++ WeightedAverage.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 float mediaPonderada(float notas[], float pesos[], int tamanho) {
-    float somaNotas = 0;
-    float somaPesos = 0;
+  float somaNotas = 0;
+  float somaPesos = 0;
 
-    for (int i = 0; i < tamanho; i++) {
-        somaNotas += notas[i] * pesos[i];
-        somaPesos += pesos[i];
-    }
+  for (int i = 0; i < tamanho; i++) {
+    somaNotas += notas[i] * pesos[i];
+    somaPesos += pesos[i];
+  }
 
-    return somaNotas / somaPesos;
+  return somaNotas / somaPesos;
 }
 
 int main() {
-    float notas[] = { 9.5, 8.0, 6.5, 9.0, 7.2 };
-    float pesos[] = { 3.0, 2.5, 2.5, 1.0, 1.0 };
-    int tamanho = sizeof(notas) / sizeof(notas[0]);
+  float notas[] = {9.5, 8.0, 6.5, 9.0, 7.2};
+  float pesos[] = {3.0, 2.5, 2.5, 1.0, 1.0};
+  int tamanho = sizeof(notas) / sizeof(notas[0]);
 
-    float resultado = mediaPonderada(notas, pesos, tamanho);
+  float resultado = mediaPonderada(notas, pesos, tamanho);
 
-    cout << "A média ponderada das notas é: " << resultado << endl;
+  cout << "A média ponderada das notas é: " << resultado << endl;
 
-    return 0;
+  return 0;
 }
