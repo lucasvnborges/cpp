ec0af47ad7137a765433a4bbd777aadc3faa2452
Lucas Borges <lucasvnborges@gmail.com>
2024-04-11 09:41:23 -0300

    adjusts code formatting with clang-format

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,27 +1,24 @@
 #include <iostream>
 
-long long factorial(int n)
-{
-    long long value = 1;
+long long factorial(int n) {
+  long long value = 1;
 
-    for (int i = 2; i <= n; i++)
-    {
-        value *= i;
-    }
+  for (int i = 2; i <= n; i++) {
+    value *= i;
+  }
 
-    return value;
+  return value;
 }
 
-int main()
-{
-    int n;
+int main() {
+  int n;
 
-    std::cout << "Enter a positive integer: ";
-    std::cin >> n;
+  std::cout << "Enter a positive integer: ";
+  std::cin >> n;
 
-    long long result = factorial(n);
+  long long result = factorial(n);
 
-    std::cout << "The factorial of " << n << " is: " << result << std::endl;
+  std::cout << "The factorial of " << n << " is: " << result << std::endl;
 
-    return 0;
+  return 0;
 }
