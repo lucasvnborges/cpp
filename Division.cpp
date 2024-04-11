e183bb4ac53b10bc10671731697ce02ea8c8b526
Lucas Borges <lucasvnborges@gmail.com>
2024-04-11 09:41:19 -0300

    adjusts code formatting with clang-format

--- Division.cpp
+++ Division.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 #include <vector>
 
-int main()
-{
-    std::vector<double> numbers = {10.1, 20.2, 30.3, 40.4};
-    double divisor = 2.3;
+int main() {
+  std::vector<double> numbers = {10.1, 20.2, 30.3, 40.4};
+  double divisor = 2.3;
 
-    for (auto &num : numbers)
-    {
-        num /= divisor;
-    }
+  for (auto &num : numbers) {
+    num /= divisor;
+  }
 
-    std::cout << "Numbers in vector after division: ";
+  std::cout << "Numbers in vector after division: ";
 
-    for (const auto &num : numbers)
-    {
-        std::cout << num << " ";
-    }
-    std::cout << std::endl;
+  for (const auto &num : numbers) {
+    std::cout << num << " ";
+  }
+  std::cout << std::endl;
 
-    return 0;
+  return 0;
 }
