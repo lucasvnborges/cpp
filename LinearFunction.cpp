879c14145522ef9aae73f3a51a1538a51f3a2932
Lucas Borges <lucasvnborges@gmail.com>
2024-04-11 09:41:50 -0300

    adjusts code formatting with clang-format

--- LinearFunction.cpp
+++ LinearFunction.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
 
-double linearFunction(double a, double b, double x)
-{
-    return a * x + b;
+double linearFunction(double a, double b, double x) {
+  return a * x + b;
 }
 
-int main()
-{
-    double a, b, x;
+int main() {
+  double a, b, x;
 
-    std::cout << "Enter the value of a: ";
-    std::cin >> a;
+  std::cout << "Enter the value of a: ";
+  std::cin >> a;
 
-    std::cout << "Enter the value of b: ";
-    std::cin >> b;
+  std::cout << "Enter the value of b: ";
+  std::cin >> b;
 
-    std::cout << "Enter the value of x: ";
-    std::cin >> x;
+  std::cout << "Enter the value of x: ";
+  std::cin >> x;
 
-    double result = linearFunction(a, b, x);
+  double result = linearFunction(a, b, x);
 
-    std::cout << "The value of the linear function f(x) = ax + b is: " << result << std::endl;
+  std::cout << "The value of the linear function f(x) = ax + b is: " << result << std::endl;
 
-    return 0;
+  return 0;
 }
